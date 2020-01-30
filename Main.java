bec6149e4c5e827fae607a89e538ecf780c01632
Ravenwulf <marcello.v.pezzella@student158.org>
2020-01-30 15:14:45 +0000

    Hotfixes

--- Main.java
+++ Main.java
@@ -2,12 +2,13 @@ class Main {
   public static void main(String[] args) {
     //Bingo nbingo = new Bingo();
 		//Blin nblin = new Blin();
-		License nlicense = new License();
+		//License nlicense = new License();
 		//Encumb nencumb = new Encumb();
 		//Line nline = new Line();
 		//WordGame nwordgame = new WordGame();
 		//StudentStart nstudentstart = new StudentStart();
 		//TestScores ntestscores = new TestScores
+		//StaplerStart nstaplerstart = new StaplerStart();
 
   }
 }
