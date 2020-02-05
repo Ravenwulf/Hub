6cfe62242fc72cf2b64b736450d6ab0e330d6ffb
Ravenwulf <marcello.v.pezzella@student158.org>
2020-02-05 16:36:45 +0000

    Added a user interface

--- Main.java
+++ Main.java
@@ -1,16 +1,51 @@
+import java.util.*;
 class Main {
   public static void main(String[] args) {
-    // Bingo nbingo = new Bingo();
-		// Blin nblin = new Blin();
-		// License nlicense = new License();
-		// Encumb nencumb = new Encumb();
-		// Line nline = new Line();
-		// WordGame nwordgame = new WordGame();
-		// StudentStart nstudentstart = new StudentStart();
-		// TestScores ntestscores = new TestScores
-		// StaplerStart nstaplerstart = new StaplerStart();
-		// LightBoardStart nlightboardstart = new LightBoardStart();
-		// SkyViewStart nskyviewstart = new SkyViewStart();
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Which program would you like to run?");
+		String input = scan.nextLine();
+		boolean going = true;
 
+		while(going){
+			switch(input){
+				case "bingo":
+				Bingo nbingo = new Bingo();
+				break;
+				case "blin":
+				Blin nblin = new Blin();
+				break;
+				case "license":
+				License nlicense = new License();
+				break;
+				case "encumb":
+				Encumb nencumb = new Encumb();
+				break;
+				case "line":
+				Line nline = new Line();
+				break;
+				case "wordgame":
+				WordGame nwordgame = new WordGame();
+				break;
+				case "student":
+				StudentStart nstudentstart = new StudentStart();
+				break;
+				case "testscores":
+				TestScores ntestscores = new TestScores();
+				break;
+				case "stapler":
+				StaplerStart nstaplerstart = new StaplerStart();
+				break;
+				case "lightboard":
+				LightBoardStart nlightboardstart = new LightBoardStart();
+				break;
+				case "skyview":
+				SkyViewStart nskyviewstart = new SkyViewStart();
+				break;
+				case "help":
+				break;
+			}
+			System.out.println("keep going? (true or false)");
+				going = scan.nextBoolean();
+		}
   }
 }
